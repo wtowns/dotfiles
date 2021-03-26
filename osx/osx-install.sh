f6a413c3556140a16cb0806b6e63b0c50f1ce710
William Towns <wtowns@gmail.com>
2021-03-26 16:24:12 -0700

    Use updated URL for homebrew

--- osx/osx-install.sh
+++ osx/osx-install.sh
@@ -33,7 +33,7 @@ fi
 
 if ! command -v brew >/dev/null 2>&1; then
 	echo "-- Installing homebrew"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo "-- Updating homebrew"
